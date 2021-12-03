1fca696abe69a05d53150dfe975bc88beb8e127d
Roy Spoolder <roy-spoolder@live.nl>
2021-12-03 10:11:17 +0100

    Puzzle day 3 part 1

---
+++ day_3_1.py
@@ -0,0 +1,31 @@
+filename = 'input/day3.txt'
+with open(filename) as file:
+    lines = file.readlines()
+    diagnostics = [line.rstrip() for line in lines]
+
+def split(word):
+    return [char for char in word]
+
+bytes = {}
+for i in range(len(diagnostics[0])) :
+    bytes[i] = {0:0, 1:0}
+
+for diagnostic in diagnostics:
+    diagnosticBytes = split(diagnostic)
+    index = 0
+    for diagnosticByte in diagnosticBytes:
+        bytes[index][int(diagnosticByte)] += 1
+        index += 1
+
+gamma = ""
+epsilon = ""
+for byte in bytes:
+    gamma += str(max(bytes[byte], key=bytes[byte].get) )
+    epsilon += str(min(bytes[byte], key=bytes[byte].get) )
+
+gammaRate = int(gamma, 2)
+epsilonRate = int(epsilon, 2)
+
+powerConsumption = gammaRate * epsilonRate
+
+print(powerConsumption)
