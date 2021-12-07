48dbed80b323d39f63de708670f70fdd4b002512
RoySp <roy-spoolder@live.nl>
2021-12-07 12:16:27 +0100

    Puzzle day 6 part 2

---
+++ day_6_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+filename = 'input/day6.txt'
+ages = {}
+with open(filename) as file:
+    for age in file.read().split(','): 
+        ages[int(age)] = ages.get(int(age), 0) + 1
+
+for day in range(256):
+    temp = {}
+    temp[6] = temp[8] = (0 if 0 not in ages else ages[0])
+    for y in range(1, 9):
+        temp[y-1] = temp.get(y-1, 0) + ages.get(y, 0)
+    ages = temp
+print(sum(ages.values()))
