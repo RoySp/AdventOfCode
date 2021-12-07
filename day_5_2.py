b996372f317e5464867e6bb1f0c201e912e627e1
RoySp <roy-spoolder@live.nl>
2021-12-07 16:02:14 +0100

    Puzzle day 5 part 2

--- day_5_2.py
+++ day_5_2.py
@@ -7,4 +7,13 @@ with open(filename) as file:
         for longitude in range(min(start_position[0], end_position[0]), max(start_position[0], end_position[0]) + 1):
             for latitude in range(min(start_position[1], end_position[1]), max(start_position[1], end_position[1]) + 1):
                 vents[(longitude, latitude)] = (vents[(longitude, latitude)] + 1) if (longitude, latitude) in vents else 1
+
+    for start_position, end_position in filter(lambda pos: pos[0][0] != pos[1][0] and pos[0][1] != pos[1][1], positions):
+        start, end = sorted([start_position, end_position], key = lambda x: x[0])
+        diagonal = 1 if start[1] < end[1] else -1
+        latitude = start[1]
+        for longitude in range(start[0], end[0] + 1):
+            vents[(longitude, latitude)] = (vents[(longitude, latitude)] + 1) if (longitude, latitude) in vents else 1
+            latitude += diagonal
+
     print(len(list(filter(lambda c: int(c[1]) >= 2, vents.items()))))
